f6b6a1b6b0d7ce44bfba9f56c707122f80bdfc5b
imran <imkhalifa28@gmail.com>
2018-10-10 23:35:51 -0400

    sere edited

--- prac/myfirstapp/serializers.py
+++ prac/myfirstapp/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework.serializers import  ModelSerializer
 from .models import Stock
 
+
 class StockSerializers(ModelSerializer):
     class Meta:
         model = Stock
         fields = '__all__'
+
+
+
