dc5482ab76fceb6472f57a4bc449ce689e36976a
imran <imkhalifa28@gmail.com>
2018-10-10 23:02:30 -0400

    models is up to date

--- prac/myfirstapp/models.py
+++ prac/myfirstapp/models.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+#Create your models here.
 class  Stock(models.Model):
-    ticker = models.CharField(max_length = 10)
-    open   = models.FloatField()
-    close  = models.FloatField()
-    volume = models.FloatField()
+     ticker = models.CharField(max_length = 10)  # type: CharField
+     open   = models.FloatField()
+     close  = models.FloatField()
+     volume = models.FloatField()
 
-    def __str__(self):
+     def __str__(self):
         return self.ticker
+
+# generating new models(assignment given by python...)
+
